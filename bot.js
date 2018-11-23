5283d6d0c84bd10e89706b48342aa2bf0800d031
KaminoShimobe <AbdulKirby@gmail.com>
2018-11-23 00:08:34 -0600

    cancelled antispam

--- bot.js
+++ bot.js
@@ -12,18 +12,18 @@ const bot = new Discord.Client({disableEveryone: true})
 
 const antispam = require("discord-anti-spam");
  
-antispam(bot, {
-  warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
-  maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
-  interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
-  warningMessage: "Stop spamming BOI", // Warning message send to the user indicating they are going to fast.
-  banMessage: "has been banned for spamming, anyone else?", // Ban message, always tags the banned user in front of it.
-  maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
-  maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
-  deleteMessagesAfterBanForPastDays: 7, // Delete the spammed messages after banning for the past x days.
-  exemptRoles: ["Dad", "Archangel"] // The names of the roles which should not be spam-filtered
+// antispam(bot, {
+//   warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
+//   maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
+//   interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
+//   warningMessage: "Stop spamming BOI", // Warning message send to the user indicating they are going to fast.
+//   banMessage: "has been banned for spamming, anyone else?", // Ban message, always tags the banned user in front of it.
+//   maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
+//   maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
+//   deleteMessagesAfterBanForPastDays: 7, // Delete the spammed messages after banning for the past x days.
+//   exemptRoles: ["Dad", "Archangel"] // The names of the roles which should not be spam-filtered
  
-});
+// });
 	
 
 bot.on("ready", async () => {
