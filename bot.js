30a86a0a04b6f0a03566fae7012154b350a03eb7
KaminoShimobe <AbdulKirby@gmail.com>
2019-01-24 10:49:19 -0600

    Adding money between bots

--- bot.js
+++ bot.js
@@ -124,6 +124,33 @@ bot.on("message", async message => {
 	let command = messageArray[0];
 
 	let args = messageArray.slice(1);
+	
+	if(message.author.bot == true && command === "!ADD" && messageArray[2] != undefined){
+		let other = message.mentions.users.first();
+		con.query(`SELECT * FROM user WHERE id = '${other.id}'`, (err, rows) => {
+		if(err) throw err;
+		let sql;
+		let money = rows[0].money;
+		var funds = parseInt(messageArray[2]);	
+		if(rows.length < 1) {
+			
+			message.channel.send("This person doesn't have a KSRPG account!");
+			return;
+		}	else {
+
+			
+			sql = `UPDATE user SET money = ${money + funds} WHERE id = '${other.id}'`;
+         
+       			 con.query(sql); 
+           			message.channel.send(other.username + " transferred $" + funds + " to their KSRPG account!");
+
+			
+			return;
+		}
+
+
+		});
+	}
 
 	if(message.author.bot) return;
 	
@@ -2846,6 +2873,36 @@ if(command === `${prefix}ORA`){
 		message.reply("You do not have rights to that.");
 	}
 }
+	
+if(command === `${prefix}add` && messageArray[1] != undefined){
+		
+		con.query(`SELECT * FROM user WHERE id = '${message.author.id}'`, (err, rows) => {
+		if(err) throw err;
+		let sql;
+		let money = rows[0].money;
+		var funds = parseInt(messageArray[1]);	
+	
+		if(rows.length < 1) {
+			
+			message.channel.send("This person doesn't have a user account!");
+			return;
+		}	else {
+
+			if(money > funds && Number.isInteger(funds) === true && funds > 0){
+			sql = `UPDATE user SET money = ${money - funds} WHERE id = '${other.id}'`;
+         
+       			 con.query(sql); 
+           		message.channel.send(">ADD " + message.author + funds);
+
+			} else{
+				message.channel.send("Invalid Input.");
+			}
+			return;
+		}
+
+
+		});
+	}		
 
 	if(command === `${prefix}beat`){
 
