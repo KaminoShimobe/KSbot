3f3f9b15cff2ae33b9d2801fbb4405640f4fdf0c
KaminoShimobe <AbdulKirby@gmail.com>
2019-10-31 09:12:12 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,7 +80,7 @@ bot.on("ready", async () => {
 			.setTitle("Update Live!")
 			.setColor("#1f3c5b")
 			.setTimestamp()
-			.setFooter("Version 1.3.10", bot.user.avatarURL);
+			.setFooter("Version 1.3.11", bot.user.avatarURL);
 	me.send(yeet);
 	
 	con.query(`SELECT * FROM user`, (err, rows) => {
