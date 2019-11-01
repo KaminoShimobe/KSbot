7706376a1e2cbd28f9a3159cb37a952d8815bd93
KaminoShimobe <AbdulKirby@gmail.com>
2019-11-01 11:09:08 -0500

    poll command

--- bot.js
+++ bot.js
@@ -4777,16 +4777,43 @@ function whom(){
 	
 function poll(){
 	const whereIam = message.channel;
+	var poll = message.content;
+	var msg = poll.replace(prefix +"poll", "");
+	var upVote = 0;
+	var downVote = 0;
+	var total = 0;
+	let note = new Discord.RichEmbed()
+
+			
+			.setTitle(message.author.username + "asks")
+			.setDescription(msg)
+			.setColor("#af25f5")
+			.setFooter("React with ✅ to stop", message.author.avatarURL)
+			.setTimestamp();
 	
-whereIam.send("Boi just react god damn.").then(sentEmbed => {
+whereIam.send(note).then(sentEmbed => {
     sentEmbed.react("👍")
     sentEmbed.react("👎")
 })
+const collector = new Discord.ReactionCollector(message.channel, m => m.author.id === message.author.id, { time: 100000000 });	
+bot.on('messageReactionAdd', (reaction, user) => {
+    if(reaction.emoji.name === "👍") {
+        upVote += 1;
+	total += 1;
+    } else if(reaction.emoji.name === "👎") {
+        downVote += 1;
+	total += 1;
+    } else if(reaction.emoji.name === "✅") {
+        collector.stop('Creator ended Poll.')
+	var yay = upVote / total;
+	var nay = downVote / total;    
+	 whereIam.send(yay + "% of people agree with \ **" + msg +  "** while " + nay + "% of people disagree.");
+    }
+});	
 	
 	
-	
-	
-}	
+}
+
 	
 function ball8(){
 	
