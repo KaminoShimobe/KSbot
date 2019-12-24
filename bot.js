ee454b5ec847b2387e8fed040805f98f1855de2b
KaminoShimobe <AbdulKirby@gmail.com>
2019-12-24 14:27:19 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -475,8 +475,17 @@ function holidayCard(){
 
 						    Jimp.loadFont(Jimp.FONT_SANS_14_WHITE).then(font => {
 						     image.resize(Jimp.AUTO, 250);
- 							 image.print(font, 20, 20, message.content).getBuffer(Jimp.MIME_JPEG, onBuffer)
- 							 image.print(font, 200, 200, `- ${message.author.username}`).getBuffer(Jimp.MIME_JPEG, onBuffer)
+ 							 image.print(
+    font,
+    10,
+    10,
+    message.context,
+    250,
+    (err, image, { x, y }) => {
+      image.print(font, x + 150, y + 200, '-' + message.author.username, 250);
+    }
+  ).getBuffer(Jimp.MIME_JPEG, onBuffer)
+
 							 image.write("holidayCard.png");
 							 person.send(`You got a holiday card!`, { files: ["holidayCard.png"] })
 							 message.author.send("Holiday Card sent to " + person.username + "!");
