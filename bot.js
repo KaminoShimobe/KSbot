84adc91fa73cb7992ead86b2c3cc2c0007294bf2
KaminoShimobe <AbdulKirby@gmail.com>
2019-12-24 15:16:40 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -422,7 +422,7 @@ con.query(`SELECT * FROM user WHERE id = '${message.author.id}'`, (err, rows) =>
 
 }	
 	
-function holidayCard(){
+function kaminoCard(){
 	
 	con.query(`SELECT * FROM user WHERE id = '${message.author.id}'`, (err, rows) => {
 		if(err) throw err;
@@ -512,11 +512,180 @@ function holidayCard(){
 	
 if(command === `!card`){
 	if(message.author.id == '242118931769196544'){
-		holidayCard();
+		kaminoCard();
 
 	}
 
 }	
+
+function holidayPfp(){
+	
+	con.query(`SELECT * FROM user WHERE id = '${message.author.id}'`, (err, rows) => {
+		if(err) throw err;
+
+		if(rows.length < 1) {
+			message.reply("You have no user!");
+			console.log(rows);
+			return;
+		}
+
+		let gift = rows[0].gift;
+		
+// 		if(gift < 10) {
+// 			message.reply("Not enough gifts!");
+// 			return;
+// 		}
+
+		
+	message.author.send("Send the image and message for the card! !cancel to cancel.");
+				const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 100000000 });
+	        		collector.once('collect', message => {
+					if (message.content == `!cancel`) {
+	               		 		message.channel.send("Cancelled.");
+	                			return;
+	            			}   else {
+// 	            		sql = `UPDATE user SET gift = ${gift - 10} WHERE id = '${message.author.id}'`;
+// 						con.query(sql);		
+						var gif = 'https://media1.giphy.com/media/tIHktzgRi8yjIplFVI/giphy_s.gif?cid=790b7611988a2c0c7bcdde449e03501ccdd50203f044ec2e&rid=giphy_s.gif'
+						var pfp = message.author.avatarURL;
+						Jimp.read(pfp)
+						  .then(image => {
+						  	function onBuffer(err, buffer) {
+      							if (err) throw err;
+      							console.log(buffer);
+    						}
+    							
+
+    						image.resize(480, 480);	
+
+						    Jimp.read(gif)
+						  .then(overlay => {
+						   overlay.composite(image, 0, 0)
+						   overlay.write("holidayAvatar.gif");
+						   message.author.send(`Here's your holiday avatar!`, { files: ["holidayAvatar.gif"] })
+ 							})
+						  .catch(err => {
+							console.error(err);
+						    // Handle an exception.
+						  });
+							
+							 
+							 
+							 
+						   
+						  })
+						  .catch(err => {
+							console.error(err);
+						    // Handle an exception.
+						  });
+						
+						
+					}
+					});
+					});
+		
+		
+		
+		
+		
+		
+	
+}
+
+if(command === `!pfp`){
+	if(message.author.id == '242118931769196544'){
+		holidayPfp();
+
+	}
+
+function holidayCard(){
+	
+	con.query(`SELECT * FROM user WHERE id = '${message.author.id}'`, (err, rows) => {
+		if(err) throw err;
+
+		if(rows.length < 1) {
+			message.reply("You have no user!");
+			console.log(rows);
+			return;
+		}
+
+		let gift = rows[0].gift;
+		
+		if(gift < 1) {
+			message.reply("Not enough gifts!");
+			return;
+		}
+
+		
+		message.author.send("Who would you like your card to go to? \n Send their id or !cancel");
+				const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 100000000 });
+	        		collector.once('collect', message => {
+	            		
+	            		if (message.content == `!cancel`) {
+	               		 message.author.send("Cancelled.");
+	                		return;
+	            		}   else {
+					var person = bot.users.get(message.content);
+					if(person != undefined){
+		message.author.send("Would you like to send a holiday card to " + person.username + "? \n Yes or No");
+				const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 100000000 });
+	        		collector.once('collect', message => {
+	            		
+	            		if (message.content == `Yes` || message.content == `yes` || message.content == `Y` || message.content == `y`) {
+	               		message.author.send("Send the image and message for the card! !cancel to cancel.");
+				const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 100000000 });
+	        		collector.once('collect', message => {
+					if (message.content == `!cancel`) {
+	               		 		message.channel.send("Cancelled.");
+	                			return;
+	            			}   else {
+	            		sql = `UPDATE user SET gift = ${gift - 1} WHERE id = '${message.author.id}'`;
+						con.query(sql);		
+						var img = message.attachments.first().url;
+						Jimp.read(img)
+						  .then(image => {
+						  	function onBuffer(err, buffer) {
+      							if (err) throw err;
+      							console.log(buffer);
+    						}
+    							var lngth = message.author.username.length;
+
+
+						    Jimp.loadFont(Jimp.FONT_SANS_16_BLACK).then(font => {
+						     image.resize(Jimp.AUTO, 250);
+ 							 image.print(font, 20, 10, message.content, 200).getBuffer(Jimp.MIME_JPEG, onBuffer)
+ 							 image.print(font, 100 - lngth, 230, `From: ${message.author.username}`, 200).getBuffer(Jimp.MIME_JPEG, onBuffer)
+							 image.write("holidayCard.png");
+							 person.send(`You got a holiday card!`, { files: ["holidayCard.png"] })
+							 message.author.send("Holiday Card sent to " + person.username + "!");
+						    });
+						  })
+						  .catch(err => {
+							console.error(err);
+						    // Handle an exception.
+						  });
+						
+						
+					}
+					});
+					} else {
+					message.author.send("Cancelled.");
+	                		return;
+				}
+				});	
+		} else {
+			message.author.send("User not found!.");
+			return;
+			}
+		}
+		
+		
+		});
+		
+	});	
+	
+}	
+}
 
 function notifications(){
 	con.query(`SELECT * FROM user WHERE id = '${message.author.id}'`, (err, rows) => {
@@ -548,7 +717,9 @@ function notifications(){
 
 
 	
-	
+if(command === `${prefix}buy` && messageArray[1] === `holidayCard`){
+		holidayCard();
+	}	
 
 
 if(command === `!notifs`){
@@ -4530,7 +4701,7 @@ con.query(`SELECT * FROM server WHERE id = '${message.guild.id}'`, (err, rows) =
 
 			
 			.setTitle(`KS-Bot Gift Shop (${prefix}buy [item] to purchase)`)
-			.setDescription("10 :gift:| **holidayCard**: \n Make a gift card to send to your friends! \n 25 :gift: | **holidayPfp** \n Give your avatar a holiday makeover! \n 50 :gift: | **stand** \n Choose which stand you want!")
+			.setDescription("1 :gift:| **holidayCard**: \n Make a gift card to send to your friends! \n 25 :gift: | **holidayPfp** \n Give your avatar a holiday makeover! \n 50 :gift: | **stand** \n Choose which stand you want!")
 			.setColor("#1d498e"); 
 
 		message.author.sendEmbed(shop);
@@ -8478,9 +8649,7 @@ if(command === `${prefix}user` && messageArray[1] == undefined){
 
 	}	
 	
-	if(command === `${prefix}buy` && messageArray[1] === `holidayCard`){
-		holidayCard();
-	}
+	
 	
 // 	if(command === `${prefix}buy` && messageArray[1] === `holidayPfp`){
 // 		holidayAvatar();
