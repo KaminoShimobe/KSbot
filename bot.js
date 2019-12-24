9d973a90e4103ba19da70d95a743a856a0971f89
KaminoShimobe <AbdulKirby@gmail.com>
2019-12-24 13:59:16 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -473,10 +473,9 @@ function holidayCard(){
       							console.log(buffer);
     						}
 
-						    Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then(font => {
+						    Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font => {
+						     image.resize(250, 250); 
  							 image.print(font, 20, 20, message.content + `\n - ${message.author.username}`).getBuffer(Jimp.MIME_JPEG, onBuffer)
-							 // var theCard = "holidayCard."+ image.getExtension();
-							 // console.log(theCard);
 							 image.write("holidayCard.png");
 							 person.send(`You got a holiday card!`, { files: ["holidayCard.png"] })
 							 message.author.send("Holiday Card sent to " + person.username + "!");
