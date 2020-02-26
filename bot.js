094389ab9cebcc834e10f34c34f9653445697706
KaminoShimobe <AbdulKirby@gmail.com>
2020-02-26 16:12:27 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1138,14 +1138,14 @@ if(emoji.name === "👍" && message.id === sentEmbed.id) {
 				list = ["321361732239097857", "187731596047155200", "134396759471423488", "220395823924510720", "140968958575640576", "242118931769196544"];
 				var duty = Math.floor(Math.random() * list.length);
 				if(attac > 0){
-					mafia.add(list[duty])
+					//mafia.add(list[duty])
 					attac -= 1;
 					players.splice(list[duty], 1);
 					// mafiaPlayers.remove(list[duty])
 					me.send(bot.users.get(list[duty]).username + " is a mafioso!");
 				} else if(detec > 0){
-					detectives.add(list[duty])
-					villagers.add(list[duty])
+					//detectives.add(list[duty])
+					//villagers.add(list[duty])
 					detec -=1;
 					ppl -=1;
 					players.splice(list[duty], 1);
@@ -1153,8 +1153,8 @@ if(emoji.name === "👍" && message.id === sentEmbed.id) {
 					me.send(bot.users.get(list[duty]).username + " is a detective!");
 					me.send(bot.users.get(list[duty]).username + " is also a villager!");
 				} else if(protec > 0){
-					doctors.add(list[duty])
-					villagers.add(list[duty])
+					//doctors.add(list[duty])
+					//villagers.add(list[duty])
 					protec -=1;
 					ppl -=1;
 					players.splice(list[duty], 1);
@@ -1162,8 +1162,9 @@ if(emoji.name === "👍" && message.id === sentEmbed.id) {
 					me.send(bot.users.get(list[duty]).username + " is a doctor!");
 					me.send(bot.users.get(list[duty]).username + " is also a villager!");
 				}	else {
-					villagers.add(list[duty])
+					//villagers.add(list[duty])
 					players.splice(list[duty], 1);
+					ppl -=1
 					// mafiaPlayers.remove(list[duty])
 					me.send(bot.users.get(list[duty]).username + " is a villager!");
 				}	
