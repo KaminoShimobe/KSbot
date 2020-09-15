b83c5e34531c64be48f256a7ce5a599cf0daeb0d
KaminoShimobe <AbdulKirby@gmail.com>
2020-09-15 07:13:26 -0500

    Create jk.js

---
+++ modules/jk.js
@@ -0,0 +1,37 @@
+module.exports = {
+	name: 'jk',
+	description: 'Deletes your message, has a 25% chance to backfire.',
+	execute(message, args) {
+	var heh = Math.floor(Math.random() * 4) + 1;
+    var msg = message.content.replace(prefix +"jk", "");
+        
+        function delet(){
+            
+        message.delete()
+
+            .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+
+            .catch(console.error);
+            
+        }
+        
+        if(heh === 1 || heh === 2 || heh === 3){
+        
+        
+            setTimeout(delet(), 1);
+
+         
+
+        } else {
+                
+             message.channel.send(message.author.username + ": __" + msg +  "__ \n but were they *REALLY* joking tho? :smirk:");;
+            
+        }
+
+         
+
+
+
+         return;
+	},
+};
