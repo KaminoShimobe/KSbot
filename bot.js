2f09a83e7bd04d12ef67c6afcf9b4d189a101ba9
KaminoShimobe <AbdulKirby@gmail.com>
2020-09-15 07:23:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,10 +48,10 @@ const EpitaphCD = new Set();
 
 const bot = new Discord.Client({disableEveryone: true})
 bot.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./modules').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
+  const command = require(`./modules/${file}`);
   client.commands.set(command.name, command);
 }
 
