be2288daf3d451332d0b8463dcff74debda082c3
KaminoShimobe <AbdulKirby@gmail.com>
2020-09-19 10:15:37 -0500

    Create inventory.js

---
+++ modules/inventory.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js");
+const mysql = require("mysql");
+
+module.exports = {
+	name: 'inventory',
+	description: 'View KSRPG inventory',
+	execute(message, args, con, bot) {
+	let messageArray = message.content.split(" ");
+    
+    con.query(`SELECT * FROM user WHERE id = '${message.author.id}'`, (err, rows) => {
+		if(err) throw err;
+
+		if(rows.length < 1) {
+			message.reply("You have no KS Bot account!");
+			
+			return;
+		}
+	
+		
+		let money = rows[0].money;
+		let hexcolor = rows[0].hue;
+		
+
+		con.query(`SELECT * FROM ksrpg WHERE id = '${message.author.id}'`, (err, rows) => {		
+			let inventory = rows[0].inventory;
+
+			var inven;
+            var list = inventory.split(",");
+            for(var i = 1; i < list.length; i++){
+              inven += (i) + ". " + list[i] + "\n";
+            } 
+            inven = inven.replace(undefined, "");
+           
+
+			let stats = new Discord.MessageEmbed()
+
+			
+			.setAuthor(message.author.username + "'s inventory:")
+			.setDescription(inven)
+			.setColor(hexcolor)
+			.setTimestamp()
+            .setFooter("ID: "  + message.author.id, message.author.avatarURL());
+
+		message.author.send(stats);
+
+
+		});
+		
+
+	});
+	},
+};
