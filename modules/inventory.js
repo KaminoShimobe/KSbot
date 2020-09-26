2510ed42bb39ca5722e238beb0cf76b48963164c
KaminoShimobe <AbdulKirby@gmail.com>
2020-09-26 11:34:19 -0500

    Update inventory.js

--- modules/inventory.js
+++ modules/inventory.js
@@ -30,6 +30,7 @@ module.exports = {
               inven += (i+1) + ". " + list[i] + "\n";
             } 
             inven = inven.replace(undefined, "");
+	    console.log(inven)
 	    if(inven == undefined){
 		    inven = "You have no items!";
 	    }    
