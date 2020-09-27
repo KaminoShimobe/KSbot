d9ef0fae1db11b17d81e2c4ac5e5a8699d014345
KaminoShimobe <AbdulKirby@gmail.com>
2020-09-26 21:18:12 -0500

    Update tossItem.js

--- modules/tossItem.js
+++ modules/tossItem.js
@@ -125,8 +125,11 @@ module.exports = {
             		newInven += "," + list[i];
             	}
             } 
-            newInven = newInven.replace(undefined, "")
-            
+            if(newInven == undefined){
+            	newInven = "";
+            } else {
+            	newInven = newInven.replace(undefined, "");
+            }
 
 
          if(canToss == true){
