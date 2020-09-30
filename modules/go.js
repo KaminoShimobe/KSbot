7eb4d50b6d0d0eeeade8ad07adb5482379446857
KaminoShimobe <AbdulKirby@gmail.com>
2020-09-30 11:14:10 -0500

    Update go.js

--- modules/go.js
+++ modules/go.js
@@ -383,7 +383,7 @@ for (const file of commandFiles) {
 
 			var options = Math.floor(Math.random() * 100) + 1;
 			let spawn = enemies.filter(enemy => enemy.spawnRate > 0)
-			let dungeon = location.find(location => location.name === location[location.indexOf(messageArray[1])])
+			let dungeon = location.find(location => location.name === "forest")
 			var enemySpawn = 0;
 			var enemyName;
 
