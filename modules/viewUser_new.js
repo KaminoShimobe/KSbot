0d89f46c5217cc4357aae85aecad148820bb46d0
KaminoShimobe <AbdulKirby@gmail.com>
2020-10-12 14:37:56 -0500

    Update viewUser_new.js

--- modules/viewUser_new.js
+++ modules/viewUser_new.js
@@ -109,14 +109,15 @@ module.exports = {
                         pfp.mask(mask, 0, 0);
                         image.composite(pfp, 25, 10, [Jimp.BLEND_SOURCE_OVER, 0, 0])
                         Jimp.loadFont(Jimp.FONT_SANS_16_BLACK).then(font => {
-                             image.print(font, 50, 205, message.author.username, 150)
-                             image.print(font, 225, 15, `"` + bio + `"`, 250)
-                             image.print(font, 225, 115, "Money: $" + money, 250)
-                             image.print(font, 225, 130, "Gifts: " + gifts, 250)
-                             image.print(font, 225, 145, "Achievements: " + achievement, 250)
-                             image.print(font, 225, 155, "Stand: " + stand, 250)
-                             image.print(font, 225, 170, "Spouse: " + marriage, 250)
-                             image.print(font, 225, 185, "Win Ratio: " + wins + ":" + losses, 250).write("test.png");
+                             image.print(font, 52, 215, message.author.username, 150)
+                             image.print(font, 230, 15, `"` + bio + `"`, 250)
+                             Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font => {
+                             image.print(font, 230, 40, "Money: $" + money, 250)
+                             image.print(font, 230, 80, "Gifts: " + gifts, 250)
+                             image.print(font, 230, 120, "Achievements: " + achievement, 250)
+                             image.print(font, 230, 160, "Stand: " + stand, 250)
+                             image.print(font, 230, 200, "Spouse: " + marriage, 250)
+                             image.print(font, 230, 240, "Win Ratio: " + wins + ":" + losses, 250).write("test.png");
 
 
                         
@@ -125,6 +126,7 @@ module.exports = {
 
 
             message.channel.send(`Image:`, { files: ["test.png"] })
+        })
 
         })
 
