e3def79e496454632271490eb97cd6d9eb52483d
KaminoShimobe <AbdulKirby@gmail.com>
2020-12-03 11:38:00 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -751,7 +751,7 @@ if(command === `!end` && messageArray[1] != undefined){
 }     
 
 
-  if(command === `!musicPlay` && messageArray[1] != undefined){
+  if(command === `!musicPlay`){
     if(message.author.id == '242118931769196544'){  
         bot.commands.get('musicPlay').execute(message, args, prefix);
 
