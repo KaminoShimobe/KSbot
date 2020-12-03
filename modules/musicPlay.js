0ce41f7bcafca648b11f3e2805316f3f5ffa024c
KaminoShimobe <AbdulKirby@gmail.com>
2020-12-03 11:21:12 -0600

    Create musicPlay.js

---
+++ modules/musicPlay.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const mysql = require("mysql");
+const fs = require('fs'); // file manager
+const ytdl = require('ytdl-core');
+
+module.exports = {
+	name: 'musicPlay',
+	description: 'Play a song from youtube',
+	execute(message, args, prefix) {
+		
+
+		const voiceChannel = message.member.voice.channel;
+
+		voiceChannel.join().then(connection => {
+	const stream = ytdl('https://www.youtube.com/watch?v=wLtdGU0-gGI', { filter: 'audioonly' });
+	const dispatcher = connection.play(stream);
+	
+	dispatcher.on('finish', () => voiceChannel.leave());
+})
+		
+
+	
+	},
+};
