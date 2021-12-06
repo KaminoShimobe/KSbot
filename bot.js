e31c339bd7a425fb6ce28793c33fcc607c5a11b3
KaminoShimobe <AbdulKirby@gmail.com>
2021-12-05 20:27:11 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -382,7 +382,7 @@ Bot.on('message', chatter => {
   let twitchArray = chatter.message.split(" ");
     
   if(chatter.message === '!discord' || chatter.message.indexOf("discord") != -1 || chatter.message.indexOf("Discord") != -1 || chatter.message.indexOf("Discord?") != -1 || chatter.message.indexOf("discord?") != -1) {
-    Bot.say('Join our discord here: https://discord.gg/cBfYZaMfxv')
+    Bot.say('Join our discord here: https://discord.gg/2WdWK8JBpy')
   }
 
   //  if(chatter.message === '!arena') {
