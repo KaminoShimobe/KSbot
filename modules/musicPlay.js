c80359964be308e018942b878240116988ea2f19
KaminoShimobe <AbdulKirby@gmail.com>
2022-01-05 14:48:08 -0600

    Update musicPlay.js

--- modules/musicPlay.js
+++ modules/musicPlay.js
@@ -41,9 +41,9 @@ module.exports = {
   const song = {
         title: songInfo.videoDetails.title,
         url: songInfo.videoDetails.video_url,
-        thumbnail: songInfo.videoDetails.thumbnail,
+        thumbnail: songInfo.videoDetails.thumbnails,
         length: songInfo.videoDetails.lengthSeconds,
-        author: songInfo.videoDetails.author
+        author: songInfo.videoDetails.author,
 
    };
 
@@ -81,7 +81,7 @@ module.exports = {
     var mins = ~~((duration % 3600) / 60);
     var secs = ~~duration % 60;
 
-    // Output like "1:01" or "4:03:59" or "123:03:59"
+    
     var ret = "";
 
     if (hrs > 0) {
