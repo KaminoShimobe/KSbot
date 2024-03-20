cb8ce7c155a2613a4b3f60370e282b01a69157e1
KaminoShimobe <AbdulKirby@gmail.com>
2024-03-20 14:24:07 -0500

    stringify JSON

--- deploy-commands.js
+++ deploy-commands.js
@@ -17,7 +17,7 @@ for (const folder of commandFolders) {
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
-		commands.push(command.data.toJSON());
+		commands.push(JSON.stringify(command.data));
 	}
 }
 
