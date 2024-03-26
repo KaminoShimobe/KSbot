ce800d59aa1e9742d51d72f4c0aed47f22500e86
KaminoShimobe <AbdulKirby@gmail.com>
2024-03-26 15:29:20 -0500

    maybe it has to be awaited

--- modules/user/view.js
+++ modules/user/view.js
@@ -89,7 +89,7 @@ module.exports = {
     };
     
    
-    let value = "";
+    let value = 0;
     function getMoney(callback) {
     
          return con.query(`SELECT * FROM user WHERE id = '${interaction.user.id}'`, (err, rows) => {
@@ -231,7 +231,7 @@ module.exports = {
         value = money;
         console.log("Value is now " + value)
     }
-    getMoney(setMoney);
+    await getMoney(setMoney);
     
     console.log(value);
     console.log(interaction.user.id)
