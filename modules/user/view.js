9b1a9c2e398086c5f76df3dc4739800f8a97412a
KaminoShimobe <AbdulKirby@gmail.com>
2024-03-26 09:02:56 -0500

    view command attempted

--- modules/user/view.js
+++ modules/user/view.js
@@ -2,12 +2,66 @@ const Discord = require("discord.js");
 const mysql = require("mysql");
 const Canvas = require('canvas');
 const Jimp = require('jimp');
+const { SlashCommandBuilder, EmbedBuilder, Client, GatewayIntentBits, AttachmentBuilder } = require('discord.js');
 
 module.exports = {
-    name: 'viewUser_new',
-    description: 'View your KS Bot Account',
-    execute(message, args, con, bot, prefix) {
-    let messageArray = message.content.split(" ");
+	data: new SlashCommandBuilder()
+        .setName('view')
+        .setDescription('Views stats about yourself or another user'),
+   async execute(interaction) {
+    
+    const bot = new Client({
+        intents: [
+           GatewayIntentBits.Guilds,
+           GatewayIntentBits.GuildPresences,
+           GatewayIntentBits.GuildMessages,
+           GatewayIntentBits.GuildMembers,
+           GatewayIntentBits.GuildInvites,
+           GatewayIntentBits.GuildModeration,
+           GatewayIntentBits.GuildMessageReactions,
+           GatewayIntentBits.DirectMessages,
+           GatewayIntentBits.GuildScheduledEvents,
+           GatewayIntentBits.MessageContent,
+       ], 
+   });
+   
+    var con_fig = {
+        host: "us-cdbr-iron-east-01.cleardb.net",
+        user: "bc9ba9370a9522",
+        password: process.env.MY_SQL,
+        database: "heroku_b523f37d8e76acb",
+        port: 3306
+    };
+    
+    var con;
+    
+    // function handleDisconnect() {
+    con = mysql.createConnection(con_fig);
+    con.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+          console.log('error when connecting to db:', err);
+          setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+      });   
+    
+    process.on('uncaughtException', function (err) {
+        console.log(err);
+        
+    }); 
+        
+    
+    
+    con.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+          console.log("disconnecting from user command")                        // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+           throw err;                                 // server variable configures this)
+        }
+    });
+        //    }
+    
+    // handleDisconnect();
     console.log("image time")
     const canvas = Canvas.createCanvas(500, 250);
     const ctx = canvas.getContext('2d');
@@ -21,11 +75,11 @@ module.exports = {
     
 
     
-  con.query(`SELECT * FROM user WHERE id = '${message.author.id}'`, (err, rows) => {
+  con.query(`SELECT * FROM user WHERE id = '${interaction.user.id}'`, (err, rows) => {
         if(err) throw err;
 
         if(rows.length < 1) {
-            message.reply(`You have no user! \n Type ${prefix}user to create one!`);
+            interaction.reply(`You have no user! \n Type ${prefix}user to create one!`);
             
             return;
         }
@@ -67,7 +121,7 @@ module.exports = {
             color = "#000000"
         }    
     
-    con.query(`SELECT * FROM achievements WHERE id = '${message.author.id}'`, (err, rows) => {
+    con.query(`SELECT * FROM achievements WHERE id = '${interaction.user.id}'`, (err, rows) => {
         if(err) throw err;
         
         
@@ -99,7 +153,7 @@ module.exports = {
                             image.color([{ apply: 'blue', params: [blue] }]);
                             Jimp.read('./modules/profileMask.png')
                     .then(mask => {
-                        var avatar = message.author.avatarURL({format: 'png'})
+                        var avatar = interaction.user.avatarURL({format: 'png'})
                         if(avatar == undefined){
                             avatar = "https://icon-library.com/images/blue-discord-icon/blue-discord-icon-17.jpg"
                         }
@@ -139,13 +193,13 @@ module.exports = {
                              image.print(font, 230, 140, "Spouse: " + marriage, 250)
                              image.print(font, 230, 160, "Win Ratio: " + wins + ":" + losses, 250).write("test.png");
                              //var imgBuf = image.getBuffer(Jimp.AUTO, onBuffer)
-
+                            const file = new AttachmentBuilder("test.png");
                         
                             // image.write("test.png");
 
 
         
-            message.channel.send( { files: ["test.png"] })
+            interaction.channel.send( { files: [file] })
            
         
 
@@ -169,33 +223,33 @@ module.exports = {
                     var done = tasks.replace("Get 10 Ws with 0 Ls", "complete");
                     mission = `UPDATE achievements SET tasks = '${done}', completed = ${achievement + 1} WHERE id = '${message.author.id}'`;
                     con.query(mission);
-                    message.channel.send(":star: **ACHIEVEMENT UNLOCKED** :star: \n `You think this is a game??`");
+                    interaction.send(":star: **ACHIEVEMENT UNLOCKED** :star: \n `You think this is a game??`");
                 }   
 
         if(tasks.indexOf("Get 100 Ws with 0 Ls") != -1 && wins == 100 && losses == 0){
                     var done = tasks.replace("Get 100 Ws with 0 Ls", "complete");
                     mission = `UPDATE achievements SET tasks = '${done}', completed = ${achievement + 1} WHERE id = '${message.author.id}'`;
                     con.query(mission);
-                    message.channel.send(":star: **ACHIEVEMENT UNLOCKED** :star: \n `Certified G.O.A.T of Rock Paper Scissors.`");
+                    interaction.send(":star: **ACHIEVEMENT UNLOCKED** :star: \n `Certified G.O.A.T of Rock Paper Scissors.`");
                 }   
         
         if(tasks.indexOf("Get $1M") != -1 && money >= 1000000){
                     var done = tasks.replace("Get $1M", "complete");
                     mission = `UPDATE achievements SET tasks = '${done}', completed = ${achievement + 1} WHERE id = '${message.author.id}'`;
                     con.query(mission);
-                    message.channel.send(":star: **ACHIEVEMENT UNLOCKED** :star: \n `Millionaire gang!`");
+                    interaction.send(":star: **ACHIEVEMENT UNLOCKED** :star: \n `Millionaire gang!`");
                 } 
         if(tasks.indexOf("Get $10M") != -1 && money >= 10000000){
                     var done = tasks.replace("Get $10M", "complete");
                     mission = `UPDATE achievements SET tasks = '${done}', completed = ${achievement + 1} WHERE id = '${message.author.id}'`;
                     con.query(mission);
-                    message.channel.send(":star: **ACHIEVEMENT UNLOCKED** :star: \n `Multimillionaire squad!`");
+                    interaction.send(":star: **ACHIEVEMENT UNLOCKED** :star: \n `Multimillionaire squad!`");
                 } 
         if(tasks.indexOf("Get $100M") != -1 && money >= 100000000){
                     var done = tasks.replace("Get $100M", "complete");
                     mission = `UPDATE achievements SET tasks = '${done}', completed = ${achievement + 1} WHERE id = '${message.author.id}'`;
                     con.query(mission);
-                    message.channel.send(":star: **ACHIEVEMENT UNLOCKED** :star: \n `Capitalism!!!`");
+                    interaction.send(":star: **ACHIEVEMENT UNLOCKED** :star: \n `Capitalism!!!`");
                 } 
         
         });
