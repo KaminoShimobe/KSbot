13d9054cd598852956e0208702dda9b5985a94fe
KaminoShimobe <AbdulKirby@gmail.com>
2024-03-26 08:01:47 -0500

    Using Promise with user id

--- modules/utility/server.js
+++ modules/utility/server.js
@@ -134,8 +134,10 @@ module.exports = {
             cr = "No";
         }
         console.log(interaction.guild.ownerId)
-        var owner = bot.users.cache.get(interaction.guild.ownerId);
-        console.log(owner.globalName)
+        var owner = bot.users.fetch(interaction.guild.ownerId);
+        owner.then(function(id){
+            console.log(id.globalName);
+        })
 
         var supporter = "";
         
